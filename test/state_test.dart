3a2c55276f625d29f5e735388e6af45c409c6d24
Steve Cassidy <steve.cassidy@mq.edu.au>
2024-04-30 16:18:25 +1000

    Add new test

--- test/state_test.dart
+++ test/state_test.dart
@@ -24,4 +24,46 @@ void main() {
       expect(testState.getCurrentQuestion().questionText, startsWith("What"));
     });
   });
+
+
+  test('State model test status changes', () {
+    
+    final client = MockClient();
+
+    when(client.get(Uri.parse('https://stevecassidy.github.io/harry-potter-quiz-app/lib/data/questions.json')))
+      .thenAnswer((_) async => http.Response(jsonEncode(questionsJson), 200));
+
+    var testState = StateModel(client);
+
+    // need to track local state so that the listener below knows
+    // what to check for whenever it is called
+    var myState = 1;
+
+    testState.addListener(() {
+      // test depends on where we are
+      if (myState == 1) {
+        expect(testState.quizStatus, 'start');
+        myState = 2;
+        testState.startQuiz();
+      } 
+      if (myState == 2) {
+        expect(testState.quizStatus, 'in-progress');
+        myState = 3;
+        testState.advanceQuestion();
+      }
+      if (myState == 3) {
+        expect(testState.quizStatus, 'in-progress');
+        myState = 4;
+        // advance past the end
+        testState.advanceQuestion();
+        testState.advanceQuestion();
+        testState.advanceQuestion();
+        testState.advanceQuestion();
+        testState.advanceQuestion();
+      }
+      if (myState == 4) {
+        expect(testState.quizStatus, 'done');
+      }
+    });
+  });
 }
